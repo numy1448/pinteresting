a4fa905f28ab330f96333595609ec7584d1c1622
Michael Harrington <linuxchimp@gmail.com>
2013-11-14 11:42:32 -0700

    Update show view, add glyphicons, and change root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Pinteresting::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root 'pages#home'
+  root 'pins#index'
   #get "pages/home"
   get 'about' => 'pages#about'
   get 'team' => 'pages#team'
